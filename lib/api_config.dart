c53fd9653249a3ce8fd7f7ab53126ed3d47e3e22
JAY SHINDE <jay554261@gmail.com>
2026-01-12 20:03:01 +0530

    added all lite tensor flow model

--- lib/api_config.dart
+++ lib/api_config.dart
@@ -1,37 +1,5 @@
 // lib/api_config.dart
-import 'dart:io';
-import 'package:flutter/foundation.dart';
-
 class ApiConfig {
-  static String get baseUrl {
-    if (kIsWeb) {
-      // Flutter Web always runs on PC browser → use localhost
-      return "https://kisan-rakshak-mlbackend.onrender.com";
-    }
-
-    // For mobile / desktop (not web)
-    if (Platform.isAndroid) {
-      // Detect: Emulator or Physical Device
-      return _isEmulator ? "https://kisan-rakshak-mlbackend.onrender.com"     // Android Emulator
-                         : "https://kisan-rakshak-mlbackend.onrender.com"; // ✔ Physical Android Device on same WiFi
-    }
-
-    // iOS Simulator / mac / windows → localhost
-    return "http://localhost:5000";
-  }
-
-  // -------- Emulator Detector (Android Only) ----------
-  static bool get _isEmulator {
-    // These checks detect emulator characteristics
-    const possibleEmulatorIndicators = [
-      "google_sdk",
-      "sdk_gphone",
-      "sdk",
-      "emulator",
-      "x86",
-    ];
-
-    final model = Platform.environment['ANDROID_EMULATOR_ENV'] ?? "";
-    return possibleEmulatorIndicators.any((e) => model.toLowerCase().contains(e));
-  }
+  static const String baseUrl =
+      "https://kisan-rakshak-mlbackend.onrender.com";
 }
